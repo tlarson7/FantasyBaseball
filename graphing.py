1edbf5fe4085cc4bc20847e75a311864f083297e
tlarson7 <tannerl@eagleinc.com>
2023-08-20 03:49:05 -0500

    modularization of graphing.py

--- graphing.py
+++ graphing.py
@@ -3,6 +3,8 @@ import plotly.graph_objects as go
 # from pitcher_plus_team import full_df
 from pitcher_plus_ytd import full_df as new_df
 from pitcher_df_transformations import pitcher_df
+from scipy.stats import pearsonr, spearmanr
+from sklearn.metrics import mean_squared_error
 
 # fig = px.scatter(data_frame=full_df.loc[full_df['GS'] == 1],x='wOBA', y='fantasy_pts', hover_data=['Name', 'Team', 'Date'])
 # fig.show()
@@ -28,8 +30,8 @@ from pitcher_df_transformations import pitcher_df
 # fig = px.scatter(data_frame=new_df.loc[new_df['GS'] == 1],x='avg_fpts', y='fantasy_pts', hover_data=['Name', 'Team', 'Date_x'], trendline='ols')
 # fig.show()
 
-def scatter(df, x, y, hover):
-    fig = px.scatter(data_frame=df, x=x, y=y, hover_data=hover, trendline='ols')
+def scatter(df, x, y, hover, title):
+    fig = px.scatter(data_frame=df, x=x, y=y, hover_data=hover, trendline='ols', title=title)
     fig.show()
 
 
@@ -38,5 +40,36 @@ def scatter(df, x, y, hover):
 #                        hover_data = ['Name', 'Team_Abbrev', 'Date', 'Differential'], color='Diff_bool')
 # fig.show()
 
-scatter(new_df.query('GS == 1'), 'wOBA', 'fantasy_pts', ['Name', 'Team', 'Date_x'])
-scatter(new_df.query('GS == 1'), 'WAR', 'fantasy_pts', ['Name', 'Team', 'Date_x'])
+
+def r_stats(df, x, y):
+    r = pearsonr(df[x], df[y])
+    r = round(r[0], 3)
+    r2 = round(r**2, 5)
+    rmse = mean_squared_error(df[x], df[y]) ** 0.5
+    return r, r2, rmse
+
+
+def get_title_str(r, r2, rmse):
+    title_str = f"""
+    R: {r}
+    \n
+    R^2: {r2}
+    \n
+    RMSE: {rmse}
+    """
+    return title_str
+
+
+def gen_models(df, x, y, hover):
+    r, r2, rmse = r_stats(df, x, y)
+    title_str = get_title_str(r, r2, rmse)
+    scatter(df, x, y, hover, title_str)
+
+
+df = new_df.query('GS == 1')
+y = 'fantasy_pts'
+hover = ['Name', 'Team', 'Date_x']
+xlist = ['wOBA', 'WAR', 'K%', 'BB%', 'ISO', 'BABIP', 'AVG', 'OBP', 'SLG', 'wRC+', 'BsR', 'Off', 'Def']
+for x in xlist:
+    print(x)
+    gen_models(df, x, y, hover)
--- pitcher_plus_ytd.py
+++ pitcher_plus_ytd.py
@@ -2,8 +2,13 @@ import pandas as pd
 from pitcher_df_transformations import pitcher_df
 
 batting_df = pd.read_csv('tm_ytd_batting_data.csv')
+
+batting_df['K%'] = pd.to_numeric(batting_df['K%'].str.split('%',expand=True)[0])
+batting_df['BB%'] = pd.to_numeric(batting_df['BB%'].str.split('%',expand=True)[0])
+
 batting_df['YTD_Target'] = pd.to_datetime(batting_df['YTD_Target'])
 batting_df['YTD_Target'] = pd.to_datetime(batting_df.groupby('Team')['Date'].shift(-1))
 
 full_df = pd.merge(left=pitcher_df, right=batting_df, left_on=['Team_Abbrev','Date'], right_on=['Team','YTD_Target'])
+
 print(full_df)
